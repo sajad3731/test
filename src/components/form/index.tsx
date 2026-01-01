ada3949ef6474192283509858a500b9483a5ad13
sajad mahdian <sajad.mahdian@gmail.com>
2026-01-01 19:29:20 +0330

    initial commit

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useState, type FC } from "react";
 import { Tab } from "./components/tab";
 import { useForm } from "react-hook-form";
 
-export const Form = () => {
+interface FormType {
+  amount: number;
+  price: number;
+  total: number;
+}
+
+export const Form: FC = () => {
   const [selectedTab, setSelectedTab] = useState("buy");
 
   const {
@@ -10,8 +16,17 @@ export const Form = () => {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<{ name: string; email: string; age: number }>()
-  const onSubmit = (data:any) => console.log(data)
+  } = useForm<FormType>({
+    defaultValues: {
+      amount: 0,
+      price: 0,
+      total: 0,
+    },
+  });
+
+  const onSubmit = (data: FormType) => {
+    console.log(data);
+  }
 
   return (
     <div className="w-96 m-auto my-10 border rounded-lg p-3 flex flex-col gap-4">
